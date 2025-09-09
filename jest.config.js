4fcdcd4d74be3500a9bb78694f06f3291dce5794
Rohit Shrivastava <rohit.shri2389@gmail.com>
2025-09-09 14:01:27 +0530

    chore(ci): add PR validation and CI/CD workflow with Docker & Kubernetes deployment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"]
-}
+  testMatch: ["**/tests/**/*.test.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  reporters: [
+    "default",
+    "jest-github-actions-reporter"
+  ],
+};
